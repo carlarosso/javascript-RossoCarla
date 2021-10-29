1d0e0760abf376d8e55a3e486ab426b831a0bdb3
Carla <you@example.com>
2021-10-29 12:28:27 +0100

    arraging files

--- clase1-2/js.js
+++ clase1-2/js.js
@@ -1,3 +1,5 @@
+// DESAFIO UNO
+
 let NombreUsuario = prompt("Ingresá tu nombre");
 let ApellidoUsuario = prompt("Ingresá tu apellido");
 
@@ -6,3 +8,5 @@ let Usuario = NombreUsuario + ApellidoUsuario;
 console.log("Hola" + Usuario);
 
 alert("Gracias por ingresar tus datos")
+
+// DESAFIO DOS
