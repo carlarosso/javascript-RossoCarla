df36ad276e57f62d58bdbdb62795310ffefd378c
Carla <you@example.com>
2021-11-11 13:42:49 +0100

    adding new challenge - objects

--- proyecto final js/js/js.js
+++ proyecto final js/js/js.js
@@ -1,4 +1,4 @@
-let userName = prompt("Inserte su usuario");
+/* let userName = prompt("Inserte su usuario");
 let pass = parseInt(prompt("inserte su contraseña numérica"));
 
 
@@ -12,4 +12,26 @@ const alertIndex = document.querySelector(".boton-index");
 
 function alertOne(alertIndex) {
     alert("You are being redirected to Our Products page");
+} */
+
+// DESASFÍO OBJETOS
+
+
+
+function usuarios(user, edad, lugar) {
+    this.user = user,
+        this.edad = edad,
+        this.lugar = lugar;
+
+    this.persona = function() {
+        console.log("su user es: " + this.user);
+        console.log("su edad es: " + this.edad);
+        console.log("usted vive en: " + this.lugar);
+    }
 }
+
+let user = prompt("inserte usuario");
+let edad = parseInt(prompt("inserte su edad"));
+let lugar = prompt("inserte su lugar de residencia");
+
+usuarios.persona();
