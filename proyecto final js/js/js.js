565568de16353fb9f57737d64fab166241466dab
Carla <you@example.com>
2021-12-04 17:48:33 +0100

    segunda entrega proyecto final

--- proyecto final js/js/js.js
+++ proyecto final js/js/js.js
@@ -33,5 +33,39 @@ function addCart(e) {
 
     // PUSH DEL TITULO DEL PRODUCTO AL CARRITO
     cart.push(prodTitle);
-    console.log(cart)
+
+    // LOCAL STORAGE
+
+    let listaProds = localStorage.setItem("cartList", JSON.stringify(cart));
 };
+
+
+// ............... DESAFIO GENERAR HTML ................................
+
+// SELECCIONO DIV #CARRITO EN CART.HTML Y LE AGREGO CONTENIDO
+
+
+let carrito = document.getElementById("carrito");
+
+let nuevoP = document.createElement("p");
+
+nuevoP.innerHTML = `<h3>Su carrito</h3>
+
+ <p> En su carrito tiene: </p>`;
+
+carrito.appendChild(nuevoP);
+
+// LLAMO A LOCAL STORAGE
+
+let selectedProd = localStorage.getItem("cartList");
+let JSONprod = JSON.parse(selectedProd);
+
+// PUSH DATA EN NUEVO ELEMENTO "LI" DENTRO DE #LISTACOMPLETA
+
+let listaCompleta = document.getElementById("listaCompleta");
+let listaProd = document.createElement("li");
+
+listaProd.innerHTML = JSONprod;
+
+
+listaCompleta.appendChild(listaProd);
