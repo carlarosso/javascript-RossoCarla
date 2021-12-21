ff3ae2083e0730cd1089b0c72031e3f1db9e1847
Carla <you@example.com>
2021-12-21 12:06:41 +0100

    adding JQUERY y desafio HTMÑ

--- proyecto final js/js/js.js
+++ proyecto final js/js/js.js
@@ -1,71 +1,149 @@
-// ............... DESAFIO INCORPORAR EVENTOS ................................
-
-//SELECCIONO TODOS LOS BOTONES "ADD TO CART"
-
-let CartBtn = document.querySelectorAll(".addCart");
-
-// CREO ARRAY CARRITO DE COMPRAS VACÍO
-
-let cart = [];
-
-// CREO UNA VARIABLE A PARTIR DE BOTONES "ADD TO CART" Y LE AGREGO UN EVENTO
-
-for (let selection of CartBtn) {
-    selection.addEventListener("click", addCart)
-}
-
-// FUNCIÓN AGREGAR A CARRITO
-
-function addCart(e) {
-
-    // RECOLECCION DE DATOS DEL EVENTO
-    let product = e.target;
-
-    // CAPTURA DE DATOS DEL PADRE DEL BOTÓN CLICKEADO
-    let divProduct = product.parentNode;
-
-    // GUARDAR EL NOMBRE DEL PRODUCTO EN UNA VARIABLE
-    let prodTitle = divProduct.querySelector("h2").textContent;
-
-
-    // MENSAJE DE ALERTA SOBRE QUÉ PRODUCTO SE SELECCIONÓ
-    alert(`You have selected ${prodTitle}`)
-
-    // PUSH DEL TITULO DEL PRODUCTO AL CARRITO
-    cart.push(prodTitle);
-
-    // LOCAL STORAGE
-
-    let listaProds = localStorage.setItem("cartList", JSON.stringify(cart));
-};
-
 
 // ............... DESAFIO GENERAR HTML ................................
 
-// SELECCIONO DIV #CARRITO EN CART.HTML Y LE AGREGO CONTENIDO
+// CREO ARRAY DE OBJETOS - PRODUCTOS
 
+/* let productos = [{
+        id: '0',
+        nombreProducto: "Good day sunshine",
+        precio: "CHF 20",
+        imagen : 'https://i.postimg.cc/QtkK1Nc6/sunshine.jpg card-img-top img-product',
+
+    }, 
+    {
+        id: '1',
+        nombreProducto: "Willy Wonka",
+        precio: "CHF 10",
+        imagen : 'https://i.postimg.cc/HjDx0ysM/willy-wonka.jpg card-img-top',
+
+    },
+    {
+        id: '2',
+        nombreProducto: "El Argento",
+        precio: "CHF 5",
+        imagen : '',
+
+    },
+    {
+        id: '3',
+        nombreProducto: "Bedfast",
+        precio: "CHF 24",
+        imagen : 'https://i.postimg.cc/JzGXvLWn/bedfast.jpg card-img-top img-product',
+
+    },
+    {
+        id: '4',
+        nombreProducto: "Swiss dreams are made of this",
+        precio: "CHF 32",
+        imagen : 'https://i.postimg.cc/SNnb6wkq/cheese.jpg card-img-top img-product',
+
+    },
+    {
+        id: '5',
+        nombreProducto: "Guilt-free",
+        precio: "CHF 68",
+        imagen : 'https://i.postimg.cc/2SMWfKzx/guilt-free.jpg card-img-top img-product',
+
+    },
+    {
+        id: '6',
+        nombreProducto: "El Argento",
+        precio: "CHF 44",
+        imagen : 'https://i.postimg.cc/3NDNDB77/el-argento.jpg',
+    },
+    {
+        id: '7',
+        nombreProducto: "Marie Antoinette",
+        precio: "CHF 21",
+        imagen : 'https://i.postimg.cc/nh5gyBCD/marie-antoinette.jpg card-img-top',
+
+    },
+    {
+        id: '8',
+        nombreProducto: "Le macaron",
+        precio: "CHF 15",
+        imagen : 'https://i.postimg.cc/ZYVq98jN/macaron.jpg card-img-top img-product',
+    },
+    {
+        id: '9',
+        nombreProducto: "Everyday I'm waffling",
+        precio: "CHF 8", 
+        imagen: 'https://i.postimg.cc/X7GTQrDD/waffle.jpg card-img-top img-product',
+    },
+]
+
+// CREO VARIABLE CARRITO
 
 let carrito = document.getElementById("carrito");
 
-let nuevoP = document.createElement("p");
+// ITERO SOBRE CADA OBJETO DEL ARRAY Y CREO UNA CARD
 
-nuevoP.innerHTML = `<h3>Su carrito</h3>
+for (var prod of productos) {
+    carrito.innerHTML += `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h2 class="card-title">${prod.nombreProducto}</h5>
+      <p class="card-text"> Get this product only for ${prod.precio}.</p>
+      <img src=${prod.imagen} class="card-img-top" alt="...">
+    </div>
+  </div>`
+}
+ */
 
- <p> En su carrito tiene: </p>`;
-
-carrito.appendChild(nuevoP);
-
-// LLAMO A LOCAL STORAGE
-
-let selectedProd = localStorage.getItem("cartList");
-let JSONprod = JSON.parse(selectedProd);
-
-// PUSH DATA EN NUEVO ELEMENTO "LI" DENTRO DE #LISTACOMPLETA
-
-let listaCompleta = document.getElementById("listaCompleta");
-let listaProd = document.createElement("li");
-
-listaProd.innerHTML = JSONprod;
+// ............... DESAFIO JQUERY  ................................
 
 
-listaCompleta.appendChild(listaProd);
+// TRAIGO DIV CART DE CART.HTML
+
+let cart = $(".cart-section1");
+
+// CREO VARIABLE PARA BOTONES CON SELECTOR JQUERY
+
+let btns = $(".addCart");
+
+// FUNCION PARA AGREGAR AL CARRITO
+
+$(".addCart").click(function(e){
+
+    // CAPTURO EL EVENTO CLICK
+
+    let clicked = e.target; 
+
+    // CAPTURO EL TITULO DEL PRODUCTO CLICKEADO
+
+    let title = clicked.parentNode.querySelector(".products-title").textContent;
+ 
+    // APPEND UN NUEVO LI CON EL NOMBRE DEL PRODUCTO CLICKEADO
+
+    $(".cartList").append( `<li> ${title} </li> `)
+
+})
+
+
+// CODIGO SIN USAR AÚN (ESTOY EXPLORANDO)
+
+
+/* let botones = $(".addCart");
+
+for (let clickedBtn of botones) {
+    clickedBtn.addEventListener("click", addToCart)
+};
+
+function addToCart(e){
+    let btn = e.target;
+    let title = btn.parentNode.querySelector("h2").textContent;
+
+    shoppingList.push((title))
+}
+
+console.log(shoppingList)
+ */
+
+
+//show/hide shopping cart when first item is choosen or is empty
+
+/* if(cart.has(itemData).length > 0) {
+   $(this).css('opacity', 1);
+} else {
+   $(this).css('opacity', 0);
+} 
+ */
