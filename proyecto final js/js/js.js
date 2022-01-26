6575a5c6a87a937008ea0ae3442ce9807a64ead7
Carla <you@example.com>
2022-01-26 18:50:22 +0100

    trabajando en proyecto final

--- proyecto final js/js/js.js
+++ proyecto final js/js/js.js
@@ -4,189 +4,180 @@ let productos = [{
         id: '0',
         nombreProducto: "Good day sunshine",
         precio: "CHF 20",
-        imagen : 'https://i.postimg.cc/QtkK1Nc6/sunshine.jpg card-img-top img-product',
+        imagen: 'https://i.postimg.cc/QtkK1Nc6/sunshine.jpg card-img-top img-product',
 
-    }, 
+    },
     {
         id: '1',
         nombreProducto: "Willy Wonka",
-        precio: "CHF 10",
-        imagen : 'https://i.postimg.cc/HjDx0ysM/willy-wonka.jpg card-img-top',
+        precio: "CHF 25",
+        imagen: 'https://i.postimg.cc/HjDx0ysM/willy-wonka.jpg card-img-top',
 
     },
     {
         id: '2',
         nombreProducto: "El Argento",
-        precio: "CHF 5",
-        imagen : '',
+        precio: "CHF 28",
+        imagen: 'https://i.postimg.cc/3NDNDB77/el-argento.jpg',
 
     },
     {
         id: '3',
         nombreProducto: "Bedfast",
         precio: "CHF 24",
-        imagen : 'https://i.postimg.cc/JzGXvLWn/bedfast.jpg card-img-top img-product',
+        imagen: 'https://i.postimg.cc/JzGXvLWn/bedfast.jpg card-img-top img-product',
 
     },
     {
         id: '4',
         nombreProducto: "Swiss dreams are made of this",
         precio: "CHF 32",
-        imagen : 'https://i.postimg.cc/SNnb6wkq/cheese.jpg card-img-top img-product',
+        imagen: 'https://i.postimg.cc/SNnb6wkq/cheese.jpg card-img-top img-product',
 
     },
     {
         id: '5',
         nombreProducto: "Guilt-free",
-        precio: "CHF 68",
-        imagen : 'https://i.postimg.cc/2SMWfKzx/guilt-free.jpg card-img-top img-product',
+        precio: "CHF 28",
+        imagen: 'https://i.postimg.cc/2SMWfKzx/guilt-free.jpg card-img-top img-product',
 
     },
+
     {
         id: '6',
-        nombreProducto: "El Argento",
-        precio: "CHF 44",
-        imagen : 'https://i.postimg.cc/3NDNDB77/el-argento.jpg',
+        nombreProducto: "Marie Antoinette",
+        precio: "CHF 21",
+        imagen: 'https://i.postimg.cc/nh5gyBCD/marie-antoinette.jpg card-img-top',
+
     },
     {
         id: '7',
-        nombreProducto: "Marie Antoinette",
-        precio: "CHF 21",
-        imagen : 'https://i.postimg.cc/nh5gyBCD/marie-antoinette.jpg card-img-top',
-
+        nombreProducto: "Le macaron",
+        precio: "CHF 15",
+        imagen: 'https://i.postimg.cc/ZYVq98jN/macaron.jpg card-img-top img-product',
     },
     {
         id: '8',
-        nombreProducto: "Le macaron",
-        precio: "CHF 15",
-        imagen : 'https://i.postimg.cc/ZYVq98jN/macaron.jpg card-img-top img-product',
-    },
-    {
-        id: '9',
         nombreProducto: "Everyday I'm waffling",
-        precio: "CHF 8", 
+        precio: "CHF 19",
         imagen: 'https://i.postimg.cc/X7GTQrDD/waffle.jpg card-img-top img-product',
     },
 ]
 
 
+// ............... CARD FLIP ........................................
+
+
+// CREO VARIABLE CARD CON SELECTOR QUERY
+
+const card = document.querySelectorAll(".card-inner");
+
+// FUNCION PARA GIRAR CARD
+
+function flipCard() {
+
+    this.classList.toggle('is-flipped');
+
+}
+
+// ITERO SOBRE CADA CARD Y AGREGO UN EVENT LISTENER CON FUNCION ON CLICK
+
+card.forEach((card) => card.addEventListener("click", flipCard));
+
+
+
+
+
+// ............... AGREGAR PRECIO EN CADA PRODUCTO ........................
+
+let productPrices = $(".productPrice")
+
+let productTitle = $(".products-title").html();
+
+
+
+
+for (let prodSeleccionado of productos) {
+
+    let title = prodSeleccionado.nombreProducto;
+
+    /* console.log(title)
+
+    console.log(productTitle) */
+
+    if (productTitle === title) {
+
+        precioTotal = prodSeleccionado.precio;
+        productPrices.append(precioTotal)
+
+    }
+}
+
+
+
+
+
 // ............... AGREGAR ELEMENTOS AL CARRITO CON JQUERY  ................................
 
-
-// TRAIGO DIV CART DE CART.HTML
-
-let cart = $(".cartList");
 
 // CREO VARIABLE PARA BOTONES CON SELECTOR JQUERY
 
 let btns = $(".addCart");
 
+// CREO ARRAY VACIO PARA CARRITO
 
-// FUNCION PARA AGREGAR AL CARRITO
+let cart = [];
 
-btns.click(function(e){
+// LOOP DE ELEMENTOS
 
-    // CAPTURO EL EVENTO CLICK
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', (e) => {
 
-    let btnClicked = e.target; 
+        // CAPTURO EL EVENTO CLICK
 
+        let btnClicked = e.target;
 
-    // CAPTURO EL TITULO DEL PRODUCTO CLICKEADO
+        // CAPTURO EL TITULO DEL PRODUCTO CLICKEADO
 
-    let title = $(this).parent().parent(".products-title").textContent;
+        let item = btnClicked.closest('.col');
 
-
-    // APPEND UN NUEVO LI CON EL NOMBRE DEL PRODUCTO CLICKEADO
-
-    $(".cartList").append( `<li> ${title} </li> `)
-    
-
-    // GUARDAR PRODUCTOS SELECCIONADOS EN STORAGE
-
-    // CREO VARIABLE CART CON ARRAY VACIO Y .PUSH() EL ELEMENTO CLICKEADO
-
-    let cart = [];
-
-    cart.push(title)
-
-
-    // GUARDAR LOS PRODUCTOS SELECCIONADOS EN LOCALSTORAGE
-
-    localStorage.setItem("cart", cart);
-
-    console.log(localStorage)
-
-})
+        let title = item.querySelector('.products-title').textContent;
 
 
 
+        // .PUSH() EL TÍTULO DEL ELEMENTO CLICKEADO EN ARRAY CART
+
+        cart.push(title)
+
+        console.log(cart)
+
+        // GUARDO DATA DE CARRITO EN LOCALSTORAGE
+
+        let storage = localStorage.setItem('fullCart', cart)
 
 
 
-
-
-
-
-
-// ----------------------------- DESAFIO ANIMACIONES -------------------------
-
-// CREO VARIABLES PARA OCULTAR Y MOSTRAR
-
-let hideDiv = $(".hideShow");
-let showBtn = $(".showContent")
-
-// OCULTAR CONTENIDO
-hideDiv.hide();
-
-
-
-// MOSTRAR CONTENIDO ON CLICK
-
-    showBtn.click(function(e){
-        $(this).next().toggle();
-        showBtn.text(function(i, text){
-            return text === "See less" ? "See more" : "See less";
-        })
-    
     })
+}
+
+
+
+
+
+// GUARDAR PRODUCTOS SELECCIONADOS EN STORAGE
+
+// CREO VARIABLE CART CON ARRAY VACIO Y .PUSH() EL ELEMENTO CLICKEADO
+
+/* let cartFilled = [];
+
+cartFilled.push(cartList)
+ */
 
 
 
 
 
 
-
-  
-//  ---------------------------- DESAFIO AJAX
-
-// GUARDO URL EN VARIABLE
-
-let url = "https://jsonplaceholder.typicode.com/photos";
-
-$(".getImg").click(function(){
-    
-
-    $.get(url, function(data){
-        let info = data;
-        
-        for (let datos of info) {
-
-            let idIn = $("#idGet").val();
-            let idData = datos.id;
-
-            if (idIn === idData) {
-                $(".imgPlaceholder").append(`<div> 
-                            <h2> ${datos.id} </h2>
-                            <p> ${datos.title} </p>
-                           </div>`);
-
-        } else {
-            alert("try another number");
-        }
-
-        }
-    })
-})
 
 
 
@@ -207,23 +198,6 @@ $(".getImg").click(function(){
             $(this).addClass('active');
         }
 })
- */
-
-
-/* let botones = $(".addCart");
-
-for (let clickedBtn of botones) {
-    clickedBtn.addEventListener("click", addToCart)
-};
-
-function addToCart(e){
-    let btn = e.target;
-    let title = btn.parentNode.querySelector("h2").textContent;
-
-    shoppingList.push((title))
-}
-
-console.log(shoppingList)
  */
 
 
